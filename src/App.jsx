a98659a164aa295bde236dc4510b91ab5926ce2d
tkdgheee <sangho135790@gmail.com>
2025-04-17 15:59:03 +0900

    12일차 과제 제출

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <main>
       <Header />
-      <section>
+      <section className="flex flex-wrap justify-center gap-[20px] px-[20px] py-[40px] bg-black">
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,10 @@
 export default function Content({ content }) {
   return (
-    <div className="content-container">
-      <img src={content.img} alt={content.title} />
-      <span>모집중</span>
-      <div>{content.title}</div>
-      <p>{content.subtitle}</p>
+    <div className="flex flex-col items-start gap-[5px]">
+      <img className="w-[300px] rounded-[10px] mb-[3px]" src={content.img} alt={content.title} />
+      <span className="text-xs text-lime-400 border border-lime-400 rounded px-1.5 py-1">모집중</span>
+      <div className="text-xl font-semibold">{content.title}</div>
+      <p className="text-xs text-gray-500">{content.subtitle}</p>
     </div>
   );
 }
